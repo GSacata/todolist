dfde3312462be8b25affba95e6524d04f116b0a3
GSacata <gsacata@gmail.com>
2024-06-20 21:19:15 -0300

    first try at module

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodolistComponent } from './todolist/todolist.component';
+import { EmailModComponent } from './email-mod/email-mod.component';
 import { TodoapiService } from './services/todoapi.service';
+import { EmailmodapiService } from './services/emailmodapi.service';
 declare var $: any;
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, TodolistComponent
+    RouterOutlet, TodolistComponent, EmailModComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  providers: [TodoapiService]
+  providers: [
+    TodoapiService, EmailmodapiService
+  ]
 })
 
 export class AppComponent {
