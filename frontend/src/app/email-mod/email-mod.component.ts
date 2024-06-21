1bf16edc9fed9353bf5f82168fbf1e1496ba935d
GSacata <gsacata@gmail.com>
2024-06-20 21:12:39 -0300

    created frontend files

---
+++ frontend/src/app/email-mod/email-mod.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-mod',
+  standalone: true,
+  imports: [],
+  templateUrl: './email-mod.component.html',
+  styleUrl: './email-mod.component.css'
+})
+export class EmailModComponent {
+
+}
---
+++ frontend/src/app/misc/mock_emailobj.ts
@@ -0,0 +1,5 @@
+import { EmailModObj } from "../models/emailobj";
+
+export const MOCKEMAILOBJ: EmailModObj[] = [
+    {id: 1, email_address: "frontemail@angular.com", email_password: "angularpassword", email_subject: "angular subject"}
+]
