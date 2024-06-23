71396f7353722fc46032b3f2ccbf98fca2e900bc
Gabriel Sacata Aranda <gabriel.sacata@datacake.com.br>
2024-06-23 17:10:25 -0300

    created simple frontend

--- frontend/src/app/email-mod/email-mod.component.ts
+++ frontend/src/app/email-mod/email-mod.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { AppModule } from '../app.module';
+import { MOCKEMAILOBJ } from '../misc/mock_emailobj';
 
 @Component({
   selector: 'app-email-mod',
   standalone: true,
-  imports: [],
+  imports: [
+    AppModule
+  ],
   templateUrl: './email-mod.component.html',
   styleUrl: './email-mod.component.css'
 })
-export class EmailModComponent {
 
+export class EmailModComponent {
+  emailobj = MOCKEMAILOBJ;
+  selectedEmail: any;
 }
