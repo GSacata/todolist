8d21ffae6ed713c544768db9402d81e1ba0b5980
Gabriel Sacata Aranda <gabriel.sacata@datacake.com.br>
2024-06-24 20:40:52 -0300

    email working with parameters

--- backend/email_mod/views.py
+++ backend/email_mod/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
 from .models import EmailModObj
 from .serializers import EmailModObjSerializer
 
 import smtplib
 import email.message
+import json
 # Create your views here.
 
 @api_view(['GET', 'POST'])
@@ -28,7 +31,6 @@ def list_emails(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# @api_view(['GET', 'PUT', 'DELETE'])
 @api_view(['GET', 'PUT', 'DELETE'])
 def detail_email(request, pk):
     # Pega apenas uma tarefa, permite editar e deletar
@@ -50,17 +52,22 @@ def detail_email(request, pk):
             serializer.save()
 
             def sent_test_email():
-                print(f"Sent test email")
+                print(f"Preparing test email")
+                content = JSONRenderer().render(serializer.data)
+                json_content = json.loads(content)
+
                 corpo_email = """
                 <p>Email de teste</p>
                 <p>Enviado pelo seu todolist. DEV!!</p>
                 """
 
+                # corpo_email = f"\n{serializer.data.keys},\n{serializer.data.values}"
+
                 msg = email.message.Message()
-                msg['Subject'] = "Email de teste hardcoded"
-                msg['From'] = 'gsacata@gmail.com'
-                msg['To'] = 'gsacata@gmail.com'
-                password = 'cslc amxx erdi uxgu' # NÃO É A SENHA DO SEU EMAIL.
+                msg['Subject'] = "Test email from GSacata's Todolist"
+                msg['From'] = json_content["email_address"]
+                msg['To'] = json_content["email_address"]
+                password = json_content["email_password"]  # NÃO É A SENHA DO SEU EMAIL.
                 msg.add_header('Content-Type', 'text/html')
                 msg.set_payload(corpo_email)
 
@@ -69,7 +76,8 @@ def detail_email(request, pk):
                 # Login Credentials for sending the mail
                 s.login(msg['From'], password)
                 s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-                print('Email enviado')
+
+                print(f'Email sent to {json_content["email_address"]}')
             sent_test_email()
 
             return Response(serializer.data)
