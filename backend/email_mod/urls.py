a728c1941c98eef677a9de5dff258a6a7a0ed8cb
Gabriel Sacata Aranda <gabriel.sacata@datacake.com.br>
2024-06-26 11:58:28 -0300

    modifications in progress

--- backend/email_mod/urls.py
+++ backend/email_mod/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import ListEmailModObj, DetailChangeEmailModObj
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('', views.list_and_send_reminder_email),
-    path('<pk>/', views.detail_email),
-    # path('<pk>/', views.save_and_test_email),
-]
+# urlpatterns = [
+#     path('', views.list_and_send_reminder_email),
+#     path('<pk>/', views.detail_email),
+#     # path('<pk>/', views.save_and_test_email),
+# ]
+
+
+router = DefaultRouter()
+router.register(r'', ListEmailModObj)
+
+urlpatterns = router.urls
